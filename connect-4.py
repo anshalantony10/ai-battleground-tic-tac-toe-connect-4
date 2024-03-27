c3c66192c297e8b108f438a7b8ca9d02d541edfd
Anshal <anshalantony10@gmail.com>
2024-03-27 08:52:06 +0000

    tic tac toe with ai move and connect 4

---
+++ connect-4.py
@@ -0,0 +1,142 @@
+import pygame
+import sys
+import numpy as np
+import math
+
+class ConnectFour:
+    def __init__(self):
+        self.ROW_COUNT = 6
+        self.COLUMN_COUNT = 7
+
+        self.BLUE = (0,0,255)
+        self.RED = (255,0,0)
+        self.YELLOW = (255,255,0)
+        self.BLACK = (0,0,0)
+
+        self.SQUARESIZE = 100
+        self.width = self.COLUMN_COUNT * self.SQUARESIZE
+        self.height = (self.ROW_COUNT+1) * self.SQUARESIZE
+        self.size = (self.width, self.height)
+        self.RADIUS = int(self.SQUARESIZE/2 - 5)
+
+        self.screen = pygame.display.set_mode(self.size)
+       
+        pygame.init()
+        self.myfont = pygame.font.SysFont("monospace", 75)
+        self.board = self.create_board()
+        self.game_over = False
+        self.turn = 0
+        self.draw_board(self.board)
+        pygame.display.update()
+
+    def create_board(self):
+        board = np.zeros((self.ROW_COUNT,self.COLUMN_COUNT))
+        return board
+
+    def drop_piece(self, board, row, col, piece):
+        board[row][col] = piece
+
+    def is_valid_location(self, board, col):
+        return board[self.ROW_COUNT-1][col] == 0
+
+    def get_next_open_row(self, board, col):
+        for r in range(self.ROW_COUNT):
+            if board[r][col] == 0:
+                return r
+
+    def winning_move(self, board, piece):
+        # Check horizontal locations for win
+        for c in range(self.COLUMN_COUNT-3):
+            for r in range(self.ROW_COUNT):
+                if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                    return True
+
+        # Check vertical locations for win
+        for c in range(self.COLUMN_COUNT):
+            for r in range(self.ROW_COUNT-3):
+                if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                    return True
+
+        # Check positively sloped diagonals
+        for c in range(self.COLUMN_COUNT-3):
+            for r in range(self.ROW_COUNT-3):
+                if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                    return True
+
+        # Check negatively sloped diagonals
+        for c in range(self.COLUMN_COUNT-3):
+            for r in range(3, self.ROW_COUNT):
+                if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                    return True
+
+    def draw_board(self, board):
+        for c in range(self.COLUMN_COUNT):
+            for r in range(self.ROW_COUNT):
+                pygame.draw.rect(self.screen, self.BLUE, (c*self.SQUARESIZE, r*self.SQUARESIZE+self.SQUARESIZE, self.SQUARESIZE, self.SQUARESIZE))
+                pygame.draw.circle(self.screen, self.BLACK, (int(c*self.SQUARESIZE+self.SQUARESIZE/2), int(r*self.SQUARESIZE+self.SQUARESIZE+self.SQUARESIZE/2)), self.RADIUS)
+
+        for c in range(self.COLUMN_COUNT):
+            for r in range(self.ROW_COUNT):        
+                if board[r][c] == 1:
+                    pygame.draw.circle(self.screen, self.RED, (int(c*self.SQUARESIZE+self.SQUARESIZE/2), self.height-int(r*self.SQUARESIZE+self.SQUARESIZE/2)), self.RADIUS)
+                elif board[r][c] == 2: 
+                    pygame.draw.circle(self.screen, self.YELLOW, (int(c*self.SQUARESIZE+self.SQUARESIZE/2), self.height-int(r*self.SQUARESIZE+self.SQUARESIZE/2)), self.RADIUS)
+        pygame.display.update()
+
+    def play(self):
+        while not self.game_over:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+                if event.type == pygame.MOUSEMOTION:
+                    pygame.draw.rect(self.screen, self.BLACK, (0,0, self.width, self.SQUARESIZE))
+                    posx = event.pos[0]
+                    if self.turn == 0:
+                        pygame.draw.circle(self.screen, self.RED, (posx, int(self.SQUARESIZE/2)), self.RADIUS)
+                    else: 
+                        pygame.draw.circle(self.screen, self.YELLOW, (posx, int(self.SQUARESIZE/2)), self.RADIUS)
+                pygame.display.update()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pygame.draw.rect(self.screen, self.BLACK, (0,0, self.width, self.SQUARESIZE))
+
+                    # Ask for Player 1 Input
+                    if self.turn == 0:
+                        posx = event.pos[0]
+                        col = int(math.floor(posx/self.SQUARESIZE))
+
+                        if self.is_valid_location(self.board, col):
+                            row = self.get_next_open_row(self.board, col)
+                            self.drop_piece(self.board, row, col, 1)
+
+                            if self.winning_move(self.board, 1):
+                                label = self.myfont.render("Player 1 wins!!", 1, self.RED)
+                                self.screen.blit(label, (40,10))
+                                self.game_over = True
+
+                    # Ask for Player 2 Input
+                    else:               
+                        posx = event.pos[0]
+                        col = int(math.floor(posx/self.SQUARESIZE))
+
+                        if self.is_valid_location(self.board, col):
+                            row = self.get_next_open_row(self.board, col)
+                            self.drop_piece(self.board, row, col, 2)
+
+                            if self.winning_move(self.board, 2):
+                                label = self.myfont.render("Player 2 wins!!", 1, self.YELLOW)
+                                self.screen.blit(label, (40,10))
+                                self.game_over = True
+
+                    self.draw_board(self.board)
+
+                    self.turn += 1
+                    self.turn = self.turn % 2
+
+                    if self.game_over:
+                        pygame.time.wait(3000)
+
+if __name__ == "__main__":
+    game = ConnectFour()
+    game.play()
---
+++ tic-tac-toe.py
@@ -0,0 +1,265 @@
+import pygame
+import sys
+import random
+
+class TicTacToe:
+    def __init__(self):
+        pygame.init()
+
+        self.WIDTH = 600
+        self.HEIGHT = 600
+        self.LINE_WIDTH = 15
+        self.WIN_LINE_WIDTH = 15
+        self.BOARD_ROWS = 3
+        self.BOARD_COLS = 3
+        self.SQUARE_SIZE = 200
+        self.CIRCLE_RADIUS = 60
+        self.CIRCLE_WIDTH = 15
+        self.CROSS_WIDTH = 25
+        self.SPACE = 55
+
+        self.RED = (255, 0, 0)
+        self.BG_COLOR = (28, 170, 156)
+        self.LINE_COLOR = (23, 145, 135)
+        self.CIRCLE_COLOR = (239, 231, 200)
+        self.CROSS_COLOR = (66, 66, 66)
+
+        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.display.set_caption('TIC TAC TOE')
+        self.screen.fill(self.BG_COLOR)
+
+        self.board = [[None for _ in range(self.BOARD_COLS)] for _ in range(self.BOARD_ROWS)]
+
+        self.player = "X"
+        self.player = random.choice(["X", "O"])
+        self.game_over = False
+        self.winner= None
+        self.draw_lines()
+
+    def draw_lines(self):
+        pygame.draw.line(self.screen, self.LINE_COLOR, (0, self.SQUARE_SIZE), (self.WIDTH, self.SQUARE_SIZE), self.LINE_WIDTH)
+        pygame.draw.line(self.screen, self.LINE_COLOR, (0, 2 * self.SQUARE_SIZE), (self.WIDTH, 2 * self.SQUARE_SIZE), self.LINE_WIDTH)
+        pygame.draw.line(self.screen, self.LINE_COLOR, (self.SQUARE_SIZE, 0), (self.SQUARE_SIZE, self.HEIGHT), self.LINE_WIDTH)
+        pygame.draw.line(self.screen, self.LINE_COLOR, (2 * self.SQUARE_SIZE, 0), (2 * self.SQUARE_SIZE, self.HEIGHT), self.LINE_WIDTH)
+
+    def draw_figures(self):
+        for row in range(self.BOARD_ROWS):
+            for col in range(self.BOARD_COLS):
+                if self.board[row][col] == 'X':
+                    pygame.draw.line(self.screen, self.CROSS_COLOR, (col * self.SQUARE_SIZE + self.SPACE, row * self.SQUARE_SIZE + self.SQUARE_SIZE - self.SPACE), (col * self.SQUARE_SIZE + self.SQUARE_SIZE - self.SPACE, row * self.SQUARE_SIZE + self.SPACE), self.CROSS_WIDTH)
+                    pygame.draw.line(self.screen, self.CROSS_COLOR, (col * self.SQUARE_SIZE + self.SPACE, row * self.SQUARE_SIZE + self.SPACE), (col * self.SQUARE_SIZE + self.SQUARE_SIZE - self.SPACE, row * self.SQUARE_SIZE + self.SQUARE_SIZE - self.SPACE), self.CROSS_WIDTH)
+                elif self.board[row][col] == 'O':
+                    pygame.draw.circle(self.screen, self.CIRCLE_COLOR, (int(col * self.SQUARE_SIZE + self.SQUARE_SIZE // 2), int(row * self.SQUARE_SIZE + self.SQUARE_SIZE // 2)), self.CIRCLE_RADIUS, self.CIRCLE_WIDTH)
+
+    def mark_square(self, row, col, player):
+        self.board[row][col] = player
+
+    def check_win(self, player):
+        for col in range(self.BOARD_COLS):
+            if self.board[0][col] == player and self.board[1][col] == player and self.board[2][col] == player:
+                return True
+        for row in range(self.BOARD_ROWS):
+            if self.board[row][0] == player and self.board[row][1] == player and self.board[row][2] == player:
+                return True
+        if self.board[2][0] == player and self.board[1][1] == player and self.board[0][2] == player:
+            return True
+        if self.board[0][0] == player and self.board[1][1] == player and self.board[2][2] == player:
+            return True
+        return False
+
+    def check_draw(self):
+        if self.check_win('X') or self.check_win('O'):
+            return False
+        for row in range(self.BOARD_ROWS):
+            for col in range(self.BOARD_COLS):
+                if self.board[row][col] is None:
+                    return False
+        return True
+
+    def restart(self):
+        self.screen.fill(self.BG_COLOR)
+        self.draw_lines()
+        self.player = "X"
+        for row in range(self.BOARD_ROWS):
+            for col in range(self.BOARD_COLS):
+                self.board[row][col] = None
+
+    def display_winner(self, player):
+        font = pygame.font.Font(None, 50)
+        if player == 'X':
+            text = font.render('Player X wins!', True, (0, 255, 0))
+        else:
+            text = font.render('Player O wins!', True, (0, 255, 0))
+
+        self.screen.blit(text, (self.WIDTH // 2 - text.get_width() // 2, self.HEIGHT // 2 - text.get_height() // 2))
+
+    def ai_move(self):
+        # Check for winning move
+        for row in range(self.BOARD_ROWS):
+            for col in range(self.BOARD_COLS):
+                if self.board[row][col] is None:
+                    self.board[row][col] = 'O'
+                    if self.check_win('O'):
+                        return
+                    self.board[row][col] = None
+
+        # Check for blocking move
+        for row in range(self.BOARD_ROWS):
+            for col in range(self.BOARD_COLS):
+                if self.board[row][col] is None:
+                    self.board[row][col] = 'X'
+                    if self.check_win('X'):
+                        self.board[row][col] = 'O'
+                        return
+                    self.board[row][col] = None
+
+        # Make a random move
+        while True:
+            row = random.randint(0, self.BOARD_ROWS - 1)
+            col = random.randint(0, self.BOARD_COLS - 1)
+            if self.board[row][col] is None:
+                self.board[row][col] = 'O'
+                return
+
+
+    # def minimax(self, depth, is_maximizing):
+    #     if self.check_win('O'):
+    #         return {'score': 1, 'row': None, 'col': None}
+    #     elif self.check_win('X'):
+    #         return {'score': -1, 'row': None, 'col': None}
+    #     elif self.check_draw():
+    #         return {'score': 0, 'row': None, 'col': None}
+
+    #     if is_maximizing:
+    #         best_score = {'score': -float('inf'), 'row': None, 'col': None}
+    #         symbol = 'O'
+    #     else:
+    #         best_score = {'score': float('inf'), 'row': None, 'col': None}
+    #         symbol = 'X'
+
+    #     for row in range(self.BOARD_ROWS):
+    #         for col in range(self.BOARD_COLS):
+    #             if self.board[row][col] is None:
+    #                 self.board[row][col] = symbol
+    #                 current_score = self.minimax(depth + 1, not is_maximizing)
+    #                 self.board[row][col] = None
+    #                 current_score['row'] = row
+    #                 current_score['col'] = col
+
+    #                 if is_maximizing and current_score['score'] > best_score['score']:
+    #                     best_score = current_score
+    #                 elif not is_maximizing and current_score['score'] < best_score['score']:
+    #                     best_score = current_score
+
+    #     return best_score
+
+    # def minimax_alpha_beta(self, depth, is_maximizing, alpha, beta):
+    #     if self.check_win('O'):
+    #         return {'score': 1, 'row': None, 'col': None}
+    #     elif self.check_win('X'):
+    #         return {'score': -1, 'row': None, 'col': None}
+    #     elif self.check_draw():
+    #         return {'score': 0, 'row': None, 'col': None}
+
+    #     if is_maximizing:
+    #         best_score = {'score': -float('inf'), 'row': None, 'col': None}
+    #         symbol = 'O'
+    #     else:
+    #         best_score = {'score': float('inf'), 'row': None, 'col': None}
+    #         symbol = 'X'
+
+    #     for row in range(self.BOARD_ROWS):
+    #         for col in range(self.BOARD_COLS):
+    #             if self.board[row][col] is None:
+    #                 self.board[row][col] = symbol
+    #                 current_score = self.minimax_alpha_beta(depth + 1, not is_maximizing, alpha, beta)
+    #                 self.board[row][col] = None
+    #                 current_score['row'] = row
+    #                 current_score['col'] = col
+
+    #                 if is_maximizing:
+    #                     if current_score['score'] > best_score['score']:
+    #                         best_score = current_score
+    #                     alpha = max(alpha, best_score['score'])
+    #                     if beta <= alpha:
+    #                         return best_score
+    #                 else:
+    #                     if current_score['score'] < best_score['score']:
+    #                         best_score = current_score
+    #                     beta = min(beta, best_score['score'])
+    #                     if beta <= alpha:
+    #                         return best_score
+
+    #     return best_score
+
+    def game_loop(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                
+                if self.player == 'O' and not self.game_over:
+                    self.ai_move()
+                    if self.check_win(self.player):
+                        self.game_over = True
+                        self.draw_figures()
+                        self.display_winner(self.player)
+                        break
+                    self.player = 'X'
+                    self.draw_figures()
+                    
+                # elif self.player == 'X' and not self.game_over:
+                #     move = self.minimax(0, False)
+                #     if move['row'] is not None and move['col'] is not None:
+                #         self.board[move['row']][move['col']] = 'X'
+                #     if self.check_win(self.player):
+                #         self.game_over = True
+                #         self.draw_figures()
+                #         self.display_winner(self.player)
+                #         break
+                #     self.player = 'O'
+                #     self.draw_figures()
+                    
+
+                if event.type == pygame.MOUSEBUTTONDOWN and not self.game_over:
+                    mouseX = event.pos[0]
+                    mouseY = event.pos[1]
+                    clicked_row = int(mouseY // self.SQUARE_SIZE)
+                    clicked_col = int(mouseX // self.SQUARE_SIZE)
+
+                    if self.board[clicked_row][clicked_col] is None:
+                        if self.player == 'X':
+                            self.mark_square(clicked_row, clicked_col, 'X')
+                            if self.check_win(self.player):
+                                self.game_over = True
+                                self.draw_figures()
+                                self.display_winner(self.player)
+                                break
+                            self.player = 'O'
+                        elif self.player == 'O':
+                            self.mark_square(clicked_row, clicked_col, 'O')
+                            if self.check_win(self.player):
+                                self.game_over = True
+                                self.draw_figures()
+                                self.display_winner(self.player)
+                                break
+                            self.player = 'X'
+                        self.draw_figures()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_r:
+                        self.restart()
+                        self.game_over = False
+
+                if self.check_draw():
+                    font = pygame.font.Font(None, 50)
+                    text = font.render('Draw!', True, (0, 255, 0))
+                    self.screen.blit(text, (self.WIDTH // 2 - text.get_width() // 2, self.HEIGHT // 2 - text.get_height() // 2))
+                if self.game_over:
+                    self.display_winner(self.player)
+                    break
+
+            pygame.display.update()
+
+if __name__ == "__main__":
+    TicTacToe().game_loop()
